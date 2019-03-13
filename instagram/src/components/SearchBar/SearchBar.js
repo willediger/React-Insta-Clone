131e69f1cb5cd31faaf3fe8721250d8a5413901e
will Ediger <will.ediger@gmail.com>
2019-03-12 23:31:33 -0500

    added components of search bar

--- instagram/src/components/SearchBar/SearchBar.js
+++ instagram/src/components/SearchBar/SearchBar.js
@@ -3,9 +3,15 @@ import './SearchBar.css';
 
 
 const SearchBar = props => (
-  <>
-    <p>search</p>
-  </>
+  <div className="searchBar">
+    <div className="logo"></div>
+    <div className="divider"></div>
+    <div className="companyName"></div>
+    <input className="searchBox" />
+    <div className="exploreBtn"></div>
+    <div className="activityBtn"></div>
+    <div className="profileBtn"></div>
+  </div>
 );
 
 export default SearchBar;
