0caa12a4a30f69b7ed9d60319b59330ee9f4b4c8
will Ediger <will.ediger@gmail.com>
2019-03-21 01:42:31 -0500

    added state to comments to track comment added

--- instagram/src/components/Comments/Comments.js
+++ instagram/src/components/Comments/Comments.js
@@ -2,13 +2,40 @@ import React from 'react';
 import './Comments.css';
 import Comment from './Comment'
 
-const Comments = props => (
-  <div className="comments">
-    {props.comments.map(c => (
-      <Comment comment={c} key={c.text} />
-    ))}
-    <input placeholder="Add comment" />
-  </div>
-);
+class Comments extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      commentToAdd: ''
+    }
+  }
+
+  inputChangeHandler = e => {
+    this.setState({commentToAdd: e.target.value})
+  }
+
+  addComment = e => {
+
+  }
+
+  render() { 
+    return (
+      <div className="comments">
+        {this.props.comments.map(c => (
+          <Comment comment={c} key={c.text} />
+        ))}
+        
+        <form onSubmit={this.addComment} className="addComment">
+          <input 
+            name="addComment"
+            value={this.state.commentToAdd}
+            placeholder="Add Comment" 
+            onChange={this.inputChangeHandler}
+          />
+        </form>
+      </div>
+    );
+  }
+}
 
 export default Comments;
