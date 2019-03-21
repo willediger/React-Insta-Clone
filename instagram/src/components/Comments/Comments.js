dee170039db81ec08568111540368377d132b3a3
will Ediger <will.ediger@gmail.com>
2019-03-21 04:49:55 -0500

    converted comments to class component and added empty state

--- instagram/src/components/Comments/Comments.js
+++ instagram/src/components/Comments/Comments.js
@@ -2,12 +2,22 @@ import React from 'react';
 import './Comments.css';
 import Comment from './Comment'
 
-const Comments = props => (
-  <div className="comments">
-    {props.comments.map(c => (
-      <Comment comment={c} key={c.text} />
-    ))}
-  </div>
-);
+class Comments extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+    }
+  }
+  render() {
+    return (
+      <div className="comments">
+        {this.props.comments.map(c => (
+          <Comment comment={c} key={c.text} />
+        ))}
+      </div>
+    );
+  }
+} 
 
 export default Comments;
