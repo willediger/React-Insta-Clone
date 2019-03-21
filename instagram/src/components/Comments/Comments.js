c984ab3034d389d93413ebdc965955eb5c47bfd8
will Ediger <will.ediger@gmail.com>
2019-03-21 02:09:40 -0500

    moved 'add comment' logic to post.js from comments

--- instagram/src/components/Comments/Comments.js
+++ instagram/src/components/Comments/Comments.js
@@ -2,40 +2,12 @@ import React from 'react';
 import './Comments.css';
 import Comment from './Comment'
 
-class Comments extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      commentToAdd: ''
-    }
-  }
-
-  inputChangeHandler = e => {
-    this.setState({commentToAdd: e.target.value})
-  }
-
-  addComment = e => {
-
-  }
-
-  render() { 
-    return (
-      <div className="comments">
-        {this.props.comments.map(c => (
-          <Comment comment={c} key={c.text} />
-        ))}
-        
-        <form onSubmit={this.addComment} className="addComment">
-          <input 
-            name="addComment"
-            value={this.state.commentToAdd}
-            placeholder="Add Comment" 
-            onChange={this.inputChangeHandler}
-          />
-        </form>
-      </div>
-    );
-  }
-}
+const Comments = props => (
+  <div className="comments">
+    {props.comments.map(c => (
+      <Comment comment={c} key={c.text} />
+    ))}
+  </div>
+);
 
 export default Comments;
--- instagram/src/components/Posts/Post.js
+++ instagram/src/components/Posts/Post.js
@@ -1,22 +1,52 @@
 import React from 'react';
 import './Posts.css';
+import moment from 'moment'
 
 import Comments from '../Comments/Comments';
 import SocialButtons from './SocialButtons'
 
-const Post = props => {
-  const p = props.post;
-  return (
-    <div className="post">
-      <div className="post-header">
-        <img src={p.thumbnailUrl} alt={p.timestamp} className="thumbnail" />
-        <p className="username">{p.username}</p>
+class Post extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      commentToAdd: ''
+    }
+  }
+
+  inputChangeHandler = e => {
+    this.setState({commentToAdd: e.target.value})
+  }
+
+  addComment = e => {
+
+  }
+
+  render() {
+    const p = this.props.post;
+    return (
+      <div className="post">
+        <div className="post-header">
+          <img src={p.thumbnailUrl} alt={p.timestamp} className="thumbnail" />
+          <p className="username">{p.username}</p>
+        </div>
+        <img src={p.imageUrl} alt={p.timestamp} className="post-img" />
+        <div className="postMetaData">
+          <SocialButtons />
+          <p className="likesCount">{p.likes} likes</p>
+          <Comments comments={p.comments} />
+          <p className="postAge">{moment(p.timestamp, 'MMMM Do YYYY, h:mm:ss a').fromNow()}</p>
+          <form onSubmit={this.addComment} className="addComment">
+            <input 
+              name="addComment"
+              value={this.state.commentToAdd}
+              placeholder="Add Comment" 
+              onChange={this.inputChangeHandler}
+            />
+          </form>
+        </div>
       </div>
-      <img src={p.imageUrl} alt={p.timestamp} className="post-img" />
-      <SocialButtons />
-      <Comments comments={p.comments} />
-    </div>
-  )
+    )
+  }
 }
 
 export default Post;
