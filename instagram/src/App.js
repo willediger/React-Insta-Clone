4a6bb5e35376ecd7b9a2c182a9cea45f63a255b2
will Ediger <will.ediger@gmail.com>
2019-03-21 04:50:12 -0500

    added "add new comment" functionality

--- instagram/src/App.js
+++ instagram/src/App.js
@@ -15,8 +15,30 @@ class App extends Component {
     this.state = {}
   }
 
+  nextCommentId = posts => {
+    // from posts, grabs each comments array, then grabs only id's of comments, then flattens array, then grabs max of id's
+    const nextId = Math.max(...posts.map(p => p.comments.map(c => Number.parseInt(c.id))).flat()) + 1;
+    return nextId.toString();
+  }
+  
+  addNewComment = (postId, comment, username) => {
+    this.setState(prevState => {
+      let newPosts = prevState.posts;
+      let nextCommentId = this.nextCommentId(newPosts);
+      const postIdx = newPosts.findIndex(p => p.id === postId);
+      newPosts[postIdx].comments.push({id: nextCommentId, username: username, text: comment})
+      return {
+        posts: newPosts
+      }
+    })
+  }
+
   componentDidMount = () => {
-    this.setState({posts: dummyData})
+    this.setState(
+      {posts: dummyData}, 
+      //testing whether adding a new comment works
+      // () => {this.addNewComment("a", "mic check 1 2 1 2", "dummy")}
+    );
   }
 
   render() {
