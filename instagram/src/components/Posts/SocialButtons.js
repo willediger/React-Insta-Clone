50150c55c4e352f63deb81109d3a8036d1c75480
will Ediger <will.ediger@gmail.com>
2019-03-21 01:42:15 -0500

    added like/comment buttons

---
+++ instagram/src/components/Posts/SocialButtons.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Posts.css';
+
+const SocialButtons = props => (
+  <div className="socialButtons">
+    <button className="likeBtn"></button>
+    <button className="commentBtn"></button>
+  </div>
+)
+
+export default SocialButtons;
